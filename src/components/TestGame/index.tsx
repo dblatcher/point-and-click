fefcec3028fc5b8e41fdf7dce7c4cbfac6d7f9a0
dblatcher <dv.bltchr@gmail.com>
2022-04-15 12:46:23 +0100

    movement over time

--- src/components/TestGame/index.tsx
+++ src/components/TestGame/index.tsx
@@ -2,30 +2,49 @@ import { useState } from "preact/hooks";
 import { RoomData, Zone } from "../../lib/RoomData";
 import { Room } from "../Room";
 
+import { useInterval } from "../../lib/useInterval"
+
 interface Props {
     data: RoomData,
 }
 
+const speed = 2
 
 export const TestGame = ({ data }: Props) => {
 
-    const [x, setX] = useState(0)
+    const [xCurrent, setCurrentX] = useState(0)
+    const [xDestination, setXDestination] = useState(0)
+
+    useInterval(() => { 
+        moveHorizontalStep(xCurrent, xDestination, speed) 
+    }, 10)
 
     const handleRoomClick = (x, y) => {
         console.log('room click', x, y)
-        setX(x)
+        setXDestination(x)
     }
     const handleZoneClick = (zone: Zone) => {
         console.log('zone click', zone)
     }
 
+    const moveHorizontalStep = (current: number, destination: number, speed: number) => {
+
+        if (current !== destination) {
+            const distance = Math.min(speed, Math.abs(current - destination))
+            const direction = current < destination ? 1 : -1
+            setCurrentX(current + (distance * direction))
+        }
+    }
+
+
+
     return (
         <main>
-            <button onClick={() => { setX(x - 10) }}>left</button>
-            <button onClick={() => { setX(x + 10) }}>right</button>
-            <span>{x}</span>
+            <button onClick={() => { setCurrentX(xCurrent - 10) }}>left</button>
+            <button onClick={() => { setCurrentX(xCurrent + 10) }}>right</button>
+            <span>{xCurrent}</span>
             <Room
-                data={data} scale={2.5} x={x}
+                data={data} scale={2.5} x={xCurrent}
                 handleRoomClick={handleRoomClick}
                 handleZoneClick={handleZoneClick} />
         </main>
