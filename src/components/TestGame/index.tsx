7d4d6ad63589773a5f686fd28a2273b361ace4c5
dblatcher <dv.bltchr@gmail.com>
2022-04-17 18:57:49 +0100

    usePosition hook

--- src/components/TestGame/index.tsx
+++ src/components/TestGame/index.tsx
@@ -4,6 +4,7 @@ import { Room } from "../Room";
 
 import { useInterval } from "../../lib/useInterval"
 import { getViewAngleCenteredOn, clamp, locateClickInWorld } from "../../lib/util";
+import { usePosition } from "../../lib/usePosition";
 
 
 interface Props {
@@ -15,28 +16,26 @@ const speed = 2
 export const TestGame = ({ data }: Props) => {
 
     const [viewAngle, setViewAngle] = useState(0)
-    const [xCurrent, setCurrentX] = useState(data.width / 2)
-    const [xDestination, setXDestination] = useState(xCurrent)
-    const [yCurrent, setCurrentY] = useState(0)
-    const [yDestination, setYDestination] = useState(yCurrent)
+    const markerPosition = usePosition(data.width / 2, 0)
+    const markerDestination = usePosition(data.width / 2, 0)
 
     const moveHorizontalStep = (speed: number) => {
-        if (xCurrent !== xDestination) {
-            const distance = Math.min(speed, Math.abs(xCurrent - xDestination))
-            const direction = xCurrent < xDestination ? 1 : -1
-            setCurrentX(xCurrent + (distance * direction))
+        if (markerPosition.x !== markerDestination.x) {
+            const distance = Math.min(speed, Math.abs(markerPosition.x - markerDestination.x))
+            const direction = markerPosition.x < markerDestination.x ? 1 : -1
+            markerPosition.x = markerPosition.x + (distance * direction)
         }
     }
     const moveVerticalStep = (speed: number) => {
-        if (yCurrent !== yDestination) {
-            const distance = Math.min(speed, Math.abs(yCurrent - yDestination))
-            const direction = yCurrent < yDestination ? 1 : -1
-            setCurrentY(yCurrent + (distance * direction))
+        if (markerPosition.y !== markerDestination.y) {
+            const distance = Math.min(speed, Math.abs(markerPosition.y - markerDestination.y))
+            const direction = markerPosition.y < markerDestination.y ? 1 : -1
+            markerPosition.y = markerPosition.y + (distance * direction)
         }
     }
 
     const followMarker = () => {
-        setViewAngle(clamp(getViewAngleCenteredOn(xCurrent, data), 1, -1))
+        setViewAngle(clamp(getViewAngleCenteredOn(markerPosition.x, data), 1, -1))
     }
 
     useInterval(() => {
@@ -47,8 +46,8 @@ export const TestGame = ({ data }: Props) => {
 
     const handleRoomClick = (x, y) => {
         const vX = locateClickInWorld(x, viewAngle, data)
-        setXDestination(vX)
-        setYDestination(data.height - y)
+        markerDestination.x = vX
+        markerDestination.y = data.height - y
     }
     const handleHotSpotClick = (zone: Zone) => {
         console.log('hotspot click', zone.name)
@@ -58,13 +57,13 @@ export const TestGame = ({ data }: Props) => {
         <main>
             <div>
                 <button onClick={() => {
-                    setCurrentX(xCurrent - 10)
-                    setXDestination(xCurrent - 10)
+                    markerPosition.x = (markerPosition.x - 10)
+                    markerDestination.x = (markerPosition.x - 10)
                 }}>left</button>
-                <span>position {xCurrent}</span>
+                <span>position {markerPosition.x}</span>
                 <button onClick={() => {
-                    setCurrentX(xCurrent + 10)
-                    setXDestination(xCurrent + 10)
+                    markerPosition.x = markerPosition.x + 10
+                    markerDestination.x = markerPosition.x + 10
                 }}>right</button>
             </div>
 
@@ -79,7 +78,7 @@ export const TestGame = ({ data }: Props) => {
             </div>
 
             <Room
-                data={data} scale={2} markerX={xCurrent} markerY={yCurrent}
+                data={data} scale={2} markerX={markerPosition.xState} markerY={markerPosition.yState}
                 viewAngle={viewAngle}
                 handleRoomClick={handleRoomClick}
                 handleHotSpotClick={handleHotSpotClick} />
---
+++ src/lib/usePosition.ts
@@ -0,0 +1,40 @@
+import { StateUpdater, useState } from "preact/hooks";
+
+
+class Position {
+    xState: number;
+    yState: number;
+    setXState: StateUpdater<number>;
+    setYState: StateUpdater<number>;
+    constructor(xState, yState, setXState:StateUpdater<number>, setYState:StateUpdater<number>) {
+        this.xState = xState
+        this.yState = yState
+        this.setXState = setXState
+        this.setYState = setYState
+    }
+
+
+    get x() {
+        return this.xState
+    }
+
+    set x(v: number) {
+        
+        this.setXState(v)
+    }
+
+    get y() {
+        return this.yState
+    }
+
+    set y(v: number) {
+        this.setYState(v)
+    }
+
+}
+
+export function usePosition(x: number, y: number) {
+    const [xv,sx] = useState(x)
+    const [yv,sy] = useState(y)
+    return new Position(xv,yv,sx,sy)
+}
