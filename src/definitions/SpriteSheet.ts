b070b0b7e7d70590c6e0c14824f6ceaed5606507
dblatcher <dv.bltchr@gmail.com>
2022-06-04 23:48:09 +0100

    fix formatting

--- src/definitions/SpriteSheet.ts
+++ src/definitions/SpriteSheet.ts
@@ -1,28 +1,28 @@
 interface SpriteSheet {
-    id: string
-    url: string
-    rows: number
-    cols: number
-    widthScale?:number
-    heightScale?:number
+    id: string;
+    url: string;
+    rows: number;
+    cols: number;
+    widthScale?: number;
+    heightScale?: number;
 }
 
 
 interface SpriteFrame {
-    sheetId: string
-    row: number
-    col: number
+    sheetId: string;
+    row: number;
+    col: number;
 }
 
 type Direction = 'left' | 'right'
 
 interface SpriteData {
-    id: string
-    defaultDirection: Direction
+    id: string;
+    defaultDirection: Direction;
     animations: Record<string, {
-        left?: SpriteFrame[]
-        right?: SpriteFrame[]
-    }>
+        left?: SpriteFrame[];
+        right?: SpriteFrame[];
+    }>;
 }
 
 export type { SpriteSheet, SpriteFrame, SpriteData, Direction }
