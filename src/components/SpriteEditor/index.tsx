f960e4ad562cbf6b9ffe62b55abfd1c76a1c705c
dblatcher <dv.bltchr@gmail.com>
2022-06-06 00:06:09 +0100

    Sprite Edittor can save to file or to service

--- src/components/SpriteEditor/index.tsx
+++ src/components/SpriteEditor/index.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { Component, h } from "preact";
+import spriteService from "../../services/spriteService";
 import { SpriteData } from "../../definitions/SpriteSheet";
 import { cloneData } from "../../lib/clone";
+import { Sprite } from "../../lib/Sprite";
 
 type SpriteEditorState = SpriteData & {
 
@@ -33,6 +35,18 @@ export class SpriteEditor extends Component<SpriteEditorProps, SpriteEditorState
         this.state = {
             ...initialData
         }
+        this.addSpriteToService = this.addSpriteToService.bind(this)
+        this.handleSaveButton = this.handleSaveButton.bind(this)
+    }
+
+    addSpriteToService() {
+        const spriteObject = new Sprite(this.state, [])
+        spriteService.add(spriteObject)
+    }
+
+    handleSaveButton() {
+        const data = cloneData(this.state);
+        this.props.saveFunction(data)
     }
 
     render() {
@@ -40,6 +54,12 @@ export class SpriteEditor extends Component<SpriteEditorProps, SpriteEditorState
         return <article>
             <h2>Sprite Editor</h2>
             <p>{id}</p>
+
+            <button onClick={this.addSpriteToService}>Add to service</button>
+            <button onClick={this.handleSaveButton}>Save to file</button>
+
+            <h3>sprites</h3>
+            {spriteService.list().map(id => <p key={id}>{id}</p>)}
         </article>
     }
 
