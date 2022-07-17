2ef8dead276f964670941c52d8979e3a96c024c8
dblatcher <dv.bltchr@gmail.com>
2022-07-17 22:12:47 +0100

    conversation schema

--- src/definitions/Conversation.ts
+++ src/definitions/Conversation.ts
@@ -1,22 +1,30 @@
-export type Conversation = {
-    id: string;
-    branches: Record<string, ConversationBranch | undefined>;
-    currentBranch?: string;
-    defaultBrach: string;
-}
+import { z } from "zod"
 
-export type ConversationBranch = {
-    choices: ConversationChoice[];
-}
+const ConversationChoiceSchema = z.object({
+    ref: z.optional(z.string()),
+    text: z.string(),
+    sequence: z.string(),
+    nextBranch: z.optional(z.string()),
+    once: z.optional(z.boolean()),
+    disabled: z.optional(z.boolean()),
+    enablesChoices: z.optional(z.array(z.array(z.optional(z.string())))),
+    disablesChoices: z.optional(z.array(z.array(z.optional(z.string())))),
+    end: z.optional(z.boolean()),
+})
 
-export type ConversationChoice = {
-    ref?: string;
-    text: string;
-    sequence: string;
-    nextBranch?: string;
-    once?: boolean;
-    disabled?: boolean;
-    enablesChoices?: (string | undefined)[][];
-    disablesChoices?: (string | undefined)[][];
-    end?: boolean;
-}
+export type ConversationChoice = z.infer <typeof ConversationChoiceSchema>
+
+const ConversationBranchSchema = z.object({
+    choices: z.array(ConversationChoiceSchema)
+})
+
+export type ConversationBranch = z.infer<typeof ConversationBranchSchema>
+
+export const ConversationSchema = z.object({
+    id: z.string(),
+    branches: z.record(z.string(), z.optional(ConversationBranchSchema)),
+    currentBranch: z.optional(z.string()),
+    defaultBrach: z.string(),
+})
+
+export type Conversation = z.infer<typeof ConversationSchema>
