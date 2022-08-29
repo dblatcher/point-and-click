29755307da3ff16190ff94cd4c4fab4ba0e0b245
dblatcher <dv.bltchr@gmail.com>
2022-08-29 22:24:17 +0100

    include sounds in prebuilt game

--- src/routes/prebuilt-game/index.tsx
+++ src/routes/prebuilt-game/index.tsx
@@ -4,10 +4,11 @@ import style from "./style.css";
 import GamePlayer from "../../components/GamePlayer";
 import { prebuiltGameDesign } from "../../../data/fullGame";
 import { imageAssets } from "../../../data/images";
+import { soundAssets } from "../../../data/sounds";
 
 const PrebuiltGame: FunctionalComponent = () => (
   <div className={style.home}>
-    <GamePlayer prebuiltGame={prebuiltGameDesign} prebuiltImageAssets={imageAssets} />
+    <GamePlayer prebuiltGame={prebuiltGameDesign} prebuiltImageAssets={imageAssets} prebuiltSoundAssets={soundAssets} />
   </div>
 );
 
