b6f2be35666a4b0e06daf9af2406921322299eeb
dblatcher <dv.bltchr@gmail.com>
2022-10-16 23:44:41 +0100

    make the interaction table nicer

--- src/components/GameEditor/InteractionEditor/index.tsx
+++ src/components/GameEditor/InteractionEditor/index.tsx
@@ -2,10 +2,13 @@
 import { Component, h, Fragment } from "preact";
 import { GameDesign, Interaction } from "src";
 import { DeleteButton, SelectInput } from "../formControls";
-import editorStyles from '../editorStyles.module.css';
 import { cloneData } from "../../../lib/clone";
+import { listIds } from "../../../lib/util";
 import { InteractionForm } from "./InteractionForm";
 import { getTargetLists, getItemDescriptions } from "./getTargetLists";
+import editorStyles from '../editorStyles.module.css';
+import styles from './styles.module.css';
+import { icons } from "../dataEditors";
 
 interface Props {
     gameDesign: GameDesign;
@@ -22,9 +25,6 @@ interface State {
     edittedIndex?: number;
 }
 
-function listIds<T extends { id: string }>(list: T[]): string[] {
-    return list.map(_ => _.id)
-}
 
 export class InteractionEditor extends Component<Props, State> {
 
@@ -68,20 +68,6 @@ export class InteractionEditor extends Component<Props, State> {
         })
     }
 
-    renderFlagConditions(interaction: Interaction) {
-        const { flagsThatMustBeFalse, flagsThatMustBeTrue } = interaction
-        if (!flagsThatMustBeFalse && !flagsThatMustBeTrue) {
-            return <span>none</span>
-        }
-
-
-
-        return <>
-            {flagsThatMustBeFalse?.map((flag, index) => (<b key={index}>![{flag}]</b>))}
-            {flagsThatMustBeTrue?.map((flag, index) => (<b key={index}>[{flag}]</b>))}
-        </>
-    }
-
     render() {
         const { gameDesign } = this.props
         const { interactions, verbs, items, rooms } = gameDesign
@@ -92,27 +78,38 @@ export class InteractionEditor extends Component<Props, State> {
         return (
             <article>
                 <h2>Interactions</h2>
-                <table className={editorStyles.interactionTable} style={{ display: interactionUnderConstruction ? 'none' : 'table' }}>
-                    <caption>{filteredInteractions.length}/{interactions.length} interactions</caption>
+                <table className={styles.interactionTable} style={{ display: interactionUnderConstruction ? 'none' : 'table' }}>
+                    <caption>
+                        <span>{filteredInteractions.length}/{interactions.length} interactions</span>
+                        <button
+                            className={[editorStyles.button, editorStyles.plusButton].join(" ")}
+                            onClick={() =>
+                                this.setState({
+                                    edittedIndex: undefined,
+                                    interactionUnderConstruction: {}
+                                })
+                            }>add new interaction</button>
+                    </caption>
                     <thead>
                         <tr>
-                            <td>verb</td>
-                            <td>target</td>
-                            <td>item</td>
-                            <td>room</td>
-                            <td rowSpan={2}>consequences</td>
-                            <td rowSpan={2}>Flag conditions</td>
+                            <th>verb</th>
+                            <th>target</th>
+                            <th>item</th>
+                            <th>room</th>
+                            <th rowSpan={2}>consequences</th>
+                            <th rowSpan={2} style={{ width: '4em' }}>must be true</th>
+                            <th rowSpan={2} style={{ width: '4em' }}>must be false</th>
                         </tr>
                         <tr>
-                            <td>
+                            <th>
                                 <SelectInput
                                     haveEmptyOption={true}
                                     onSelect={verbFilter => { this.setState({ verbFilter }) }}
                                     emptyOptionLabel="[ANY VERB]"
                                     value={verbFilter}
                                     items={listIds(verbs)} />
-                            </td>
-                            <td>
+                            </th>
+                            <th>
                                 <SelectInput
                                     haveEmptyOption={true}
                                     onSelect={targetFilter => { this.setState({ targetFilter }) }}
@@ -121,8 +118,8 @@ export class InteractionEditor extends Component<Props, State> {
                                     items={targetLists.ids}
                                     descriptions={targetLists.descriptions}
                                 />
-                            </td>
-                            <td>
+                            </th>
+                            <th>
                                 <SelectInput
                                     haveEmptyOption={true}
                                     onSelect={itemFilter => { this.setState({ itemFilter }) }}
@@ -130,47 +127,63 @@ export class InteractionEditor extends Component<Props, State> {
                                     value={itemFilter}
                                     items={listIds(items)}
                                     descriptions={getItemDescriptions(gameDesign)} />
-                            </td>
-                            <td>
+                            </th>
+                            <th>
                                 <SelectInput
                                     haveEmptyOption={true}
                                     onSelect={roomFilter => { this.setState({ roomFilter }) }}
                                     emptyOptionLabel="[ANY ROOM]"
                                     value={roomFilter}
                                     items={listIds(rooms)} />
-                            </td>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
                         {interactions.map((interaction, index) => {
-                            return filteredInteractions.includes(interaction)
-                                ? (
-                                    <tr key={index}>
-                                        <td>{interaction.verbId}</td>
-                                        <td>
-                                            <span>{interaction.targetId}</span>
-                                            {interaction.targetStatus && <span>({interaction.targetStatus})</span>}
-                                        </td>
-                                        <td>{interaction.itemId}</td>
-                                        <td>{interaction.roomId}</td>
-                                        <td>{interaction.consequences.length}x consequences</td>
-                                        <td>{this.renderFlagConditions(interaction)}</td>
-                                        <td><DeleteButton label="delete" confirmationText="really?" onClick={() => { this.props.deleteInteraction(index) }} /></td>
-                                        <td><button onClick={() => this.setState({ edittedIndex: index, interactionUnderConstruction: cloneData(interaction) })}>edit</button></td>
-                                    </tr>
-                                )
-                                : <></>
+
+                            if (!filteredInteractions.includes(interaction)) { return <></> }
+                            const {
+                                verbId, targetId, targetStatus, itemId, roomId,
+                                consequences, flagsThatMustBeFalse = [], flagsThatMustBeTrue = []
+                            } = interaction
+
+                            const trueFlagText = flagsThatMustBeTrue.length ? `x${flagsThatMustBeTrue.length}` : ''
+                            const trueFlagTitle = flagsThatMustBeTrue.join(", ")
+
+                            const falseFlagText = flagsThatMustBeFalse.length ? `x${flagsThatMustBeFalse.length}` : ''
+                            const falseFlagTitle = flagsThatMustBeFalse.join(", ")
+
+                            const consequenceText = consequences.length ? `x${consequences.length}` : 'empty'
+                            const consequenceTitle = consequences.map(_=>_.type).join(", ")
+
+                            return (
+                                <tr key={index}>
+                                    <td>{verbId}</td>
+                                    <td>
+                                        <span>{targetId}</span>
+                                        {targetStatus && <span>({targetStatus})</span>}
+                                    </td>
+                                    <td>{itemId}</td>
+                                    <td>{roomId}</td>
+                                    <td className={styles.centered} title={consequenceTitle}>{consequenceText}</td>
+                                    <td className={styles.centered} title={trueFlagTitle}>{trueFlagText}</td>
+                                    <td className={styles.centered} title={falseFlagTitle}>{falseFlagText}</td>
+                                    <td>
+                                        <button
+                                            className={[editorStyles.button].join(" ")}
+                                            onClick={() => this.setState({ edittedIndex: index, interactionUnderConstruction: cloneData(interaction) })}>
+                                            edit
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <DeleteButton label={icons.DELETE}
+                                            className={[editorStyles.button, editorStyles.deleteButton].join(" ")}
+                                            confirmationText="really?"
+                                            onClick={() => { this.props.deleteInteraction(index) }} />
+                                    </td>
+                                </tr>
+                            )
                         })}
-                        <tr>
-                            <td colSpan={10}>
-                                <button onClick={() =>
-                                    this.setState({
-                                        edittedIndex: undefined,
-                                        interactionUnderConstruction: {}
-                                    })
-                                }>add new interaction</button>
-                            </td>
-                        </tr>
                     </tbody>
                 </table>
 
