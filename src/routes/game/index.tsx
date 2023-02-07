7d8dfac0c3f5c2a46c11a6802f7db0548c217367
dblatcher <dv.bltchr@gmail.com>
2023-02-07 23:30:05 +0000

    check in the zipped games

--- src/routes/game/index.tsx
+++ src/routes/game/index.tsx
@@ -6,6 +6,10 @@ import GamePlayer from "../../components/GamePlayer";
 const Game: FunctionalComponent = () => (
   <div className={style.home}>
     <GamePlayer />
+    <br />
+    <a href="/assets/castle-life.game.zip" download={'castle-life.game.zip'} target="_blank">download castle game file</a>
+    <br />
+    <a href="/assets/THE_TEST_GAME.game.zip" download={'THE_TEST_GAME.game.zip'} target="_blank">download test game file</a>
   </div>
 );
 
