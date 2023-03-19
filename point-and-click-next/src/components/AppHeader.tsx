2cb06d63753944a3b0630dee26d7a01d71bdc78a
dblatcher <dv.bltchr@gmail.com>
2023-03-19 12:18:32 +0000

    Page Layout component

--- point-and-click-next/src/components/AppHeader.tsx
+++ point-and-click-next/src/components/AppHeader.tsx
@@ -2,7 +2,7 @@ import { AppBar, Typography } from '@mui/material'
 import Link from 'next/link'
 
 
-export default function AppHeader() {
+export function AppHeader() {
     return (
         <AppBar component='header'>
             <Typography component="div" sx={{ flexGrow: 1 }}>
---
+++ point-and-click-next/src/components/PageLayout.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import { AppHeader } from '@/components/AppHeader'
+import { Box } from '@mui/material'
+import { ReactNode } from 'react'
+
+
+interface Props {
+    children: ReactNode
+}
+
+export function PageLayout({ children }: Props) {
+    return (
+        <>
+            <Head>
+                <title>Point and Click</title>
+                <meta name="description" content="Point and Click" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Box sx={{ display: 'flex' }} paddingTop={'2rem'}>
+                <AppHeader />
+                <Box component='main' sx={{ width: '100%' }}>
+                    {children}
+                </Box>
+            </Box>
+        </>
+    )
+}
