92e65bcb06ae56133d50d01b927c44f5e0084f10
dblatcher <dv.bltchr@gmail.com>
2023-04-01 22:18:09 +0100

    generate commandLine text better

--- point-and-click-next/src/components/game-mui-ux/CommandLine.tsx
+++ point-and-click-next/src/components/game-mui-ux/CommandLine.tsx
@@ -14,35 +14,28 @@ interface Props {
 
 export function CommandLine({ verb, item, target, hoverTarget }: Props) {
 
-    let text = '>'
-
-    if (verb) {
-        text = `${verb.label}`;
-
-        if (item) {
-            text += ` ${item.name || item.id} ${verb.preposition}`
-        }
-
-        if (target) {
-            text += ` ${target.name || target.id}`
-        }
-    }
-
-    const hoverText = hoverTarget ? hoverTarget.name || hoverTarget.id : '...';
-
     const theme = useTheme()
+    const Bold = (props: { text: string }) => <b style={{ color: theme.palette.primary.main }}>{props.text}{' '}</b>
+    const hoverText = hoverTarget ? hoverTarget.name || hoverTarget.id : '..?'
 
     return (
         <Container maxWidth={'sm'} sx={{ padding: 1 }}>
-            <Card sx={{ padding: .5, height:'2.5em' }}>
-                <Typography component={'span'}>
-                    {text}
+            <Card sx={{ padding: .5, height: '2.5em' }}>
+                <Typography component={'div'} sx={{ lineHeight: 1 }}>
+                    {verb && (
+                        <span>{verb.label}{' '}</span>
+                    )}
+                    {(verb && item) && <>
+                        <Bold text={item.name || item.id} />
+                        <span>{verb.preposition}{' '}</span>
+                    </>}
+                    {(verb && target) && (
+                        <span>{target.name || target.id}{' '}</span>
+                    )}
+                    {(!target) && (
+                        <Bold text={hoverText} />
+                    )}
                 </Typography>
-                {!target && (
-                    <Typography component={'span'} color={theme.palette.primary.main}>
-                        {' '}{hoverText}
-                    </Typography>
-                )}
             </Card>
         </Container>
     )
