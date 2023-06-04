dfd6bfcf424f3227a431fc95ca207ab6d537cd1f
dblatcher <dv.bltchr@gmail.com>
2023-06-04 15:53:31 +0100

    fix mui componet set export

--- src/components/game-mui-ux/index.ts
+++ src/components/game-mui-ux/index.ts
@@ -1,18 +1,8 @@
 import { UiComponentSet } from "../game/uiComponentSet";
-import { CommandLine } from "./CommandLine";
-import { ConversationMenu } from "./ConversationMenu";
-import { ItemMenu } from "./ItemMenu";
 import { Layout } from "./Layout";
 import { SaveMenu } from "./SaveMenu";
-import { SoundToggle } from "./SoundToggle";
-import { VerbMenu } from "./VerbMenu";
 
 export const materialUiComponents: UiComponentSet = {
-    CommandLineComponent: CommandLine,
-    VerbMenuComponent: VerbMenu,
-    ItemMenuComponent: ItemMenu,
-    SaveMenuComponent: SaveMenu,
-    ConversationMenuComponent: ConversationMenu,
-    SoundToggleComponent: SoundToggle,
     GameLayoutComponent: Layout,
+    SaveMenuComponent: SaveMenu,
 }
