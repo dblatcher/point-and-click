5edef40c9faf727dc7e284df71696bb3b3e00537
dblatcher <dv.bltchr@gmail.com>
2023-06-23 23:56:07 +0100

    room doesn't resize

--- src/components/svg/Room/index.tsx
+++ src/components/svg/Room/index.tsx
@@ -1,21 +1,20 @@
-import { CSSProperties, FunctionComponent, MouseEventHandler, ReactNode, useEffect, useRef, useState } from "react";
-import { CellMatrix } from "@/lib/pathfinding/cells";
-import { RoomData, HotspotZone } from "@/definitions";
-import { getShift } from "@/lib/roomFunctions";
-import styles from './styles.module.css';
-import Hotspot from "./HotSpot";
-import ZoneSvg from "@/components/svg/ZoneSvg";
-import ObstacleCellOverlay from "./ObstableCellOverlay";
-import BackgroundShape from "./BackgroundShape";
-import { HandleHoverFunction, RoomContentItem } from "../../game";
 import { ActorFigure } from "@/components/svg/ActorFigure";
 import { DialogueBubble } from "@/components/svg/DialogueBubble";
+import ZoneSvg from "@/components/svg/ZoneSvg";
+import { HotspotZone, RoomData } from "@/definitions";
+import { CellMatrix } from "@/lib/pathfinding/cells";
+import { getShift } from "@/lib/roomFunctions";
+import { CSSProperties, FunctionComponent, MouseEventHandler, ReactNode } from "react";
+import { HandleHoverFunction, RoomContentItem } from "../../game";
+import BackgroundShape from "./BackgroundShape";
+import Hotspot from "./HotSpot";
+import ObstacleCellOverlay from "./ObstableCellOverlay";
+import styles from './styles.module.css';
 
 interface Props {
     data: RoomData;
     maxWidth?: number;
     maxHeight?: number;
-    noResize?: boolean;
     viewAngle: number;
     handleRoomClick: { (x: number, y: number): void };
     handleHotspotClick?: { (zone: HotspotZone): void };
@@ -38,7 +37,6 @@ export const Room: FunctionComponent<Props> = ({
     data,
     maxWidth = 300,
     maxHeight = 200,
-    noResize,
     viewAngle,
     handleRoomClick,
     handleHotspotClick,
@@ -56,38 +54,12 @@ export const Room: FunctionComponent<Props> = ({
     forPreview = false,
     children,
 }: Props) => {
-    const { id, frameWidth, height, background, hotspots = [], obstacleAreas = [], walkableAreas = [] } = data;
-    const figureRef = useRef<HTMLElement>(null)
-    const [parentWidth, setParentWidth] = useState(Infinity)
-    const [parentHeight, setParentHeight] = useState(Infinity)
+    const { id, frameWidth, height, background, hotspots = [], obstacleAreas = [], walkableAreas = [], } = data;
 
-    const handleResize = (): void => {
-        const { current: figure } = figureRef
-        if (noResize) {
-            setParentWidth(Infinity)
-            setParentHeight(Infinity)
-            return
-        }
-        if (!figure || !figure.parentElement) {
-            return
-        }
-        setParentWidth(figure.parentElement.clientWidth)
-        setParentHeight(figure.parentElement.clientHeight)
-    }
-    useEffect(() => {
-        handleResize()
-        window.addEventListener('resize', handleResize)
-        return (): void => {
-            window.removeEventListener('resize', handleResize)
-        }
-    })
-
-    const figureWidth = Math.min(parentWidth, maxWidth)
-    // const figureHeight = Math.min(parentHeight, maxHeight)
     const scale = Math.min(
-        figureWidth / frameWidth, 
-        // figureHeight / height
-        )
+        maxWidth / frameWidth,
+        maxHeight / height
+    )
 
     const processRoomClick: MouseEventHandler<HTMLElement> = (event) => {
         return handleRoomClick(event.nativeEvent.offsetX / scale, event.nativeEvent.offsetY / scale)
@@ -101,114 +73,112 @@ export const Room: FunctionComponent<Props> = ({
     }
 
     return (
-        <div frame-scale={scale} parent-width={parentWidth}>
-            <figure ref={figureRef}
-                className={styles.roomFigure}
-                style={figureInlineStyle}
-                onClick={processRoomClick}
-            >
+        <figure
+            className={styles.roomFigure}
+            style={figureInlineStyle}
+            onClick={processRoomClick}
+        >
 
-                <svg xmlns="http://www.w3.org/2000/svg"
-                    className={styles.roomSvg}
-                    viewBox={`0 0 ${frameWidth} ${height}`}>
+            <svg xmlns="http://www.w3.org/2000/svg"
+                className={styles.roomSvg}
+                viewBox={`0 0 ${frameWidth} ${height}`}>
 
-                    {background
-                        .filter(layer => layer.parallax <= 1)
-                        .map((layer, index) =>
-                            <BackgroundShape key={index}
-                                layer={layer}
-                                viewAngle={viewAngle}
-                                roomData={data}
-                            />
-                        )}
-
-                    {showObstacleAreas && walkableAreas.map((zone, index) => {
-                        const center = (frameWidth / 2) + getShift(viewAngle, 1, data)
-                        const left = center - data.width / 2
-                        const classes = zone.disabled ? [styles.walkableArea, styles.disabledZone].join(" ") : styles.walkableArea;
-                        return <ZoneSvg key={index}
-                            className={classes}
-                            stopPropagation={false}
-                            zone={zone}
-                            x={zone.x + left}
-                            y={data.height - zone.y}
-                            markVertices={markWalkableVertices.includes(index)}
-                        />
-                    })}
-
-                    {showObstacleAreas && obstacleAreas.map((zone, index) => {
-                        const center = (frameWidth / 2) + getShift(viewAngle, 1, data)
-                        const left = center - data.width / 2
-                        const classes = zone.disabled ? [styles.obstacleArea, styles.disabledZone].join(" ") : styles.obstacleArea;
-                        return <ZoneSvg key={index}
-                            className={classes}
-                            stopPropagation={false}
-                            zone={zone}
-                            x={zone.x + left}
-                            y={data.height - zone.y}
-                            markVertices={markObstacleVertices.includes(index)}
-                        />
-                    })}
-
-                    {obstacleCells &&
-                        <ObstacleCellOverlay roomData={data} viewAngle={viewAngle} cellMatrix={obstacleCells} />
-                    }
-
-                    {hotspots.map((zone, index) =>
-                        <Hotspot key={index}
-                            zone={zone}
+                {background
+                    .filter(layer => layer.parallax <= 1)
+                    .map((layer, index) =>
+                        <BackgroundShape key={index}
+                            layer={layer}
                             viewAngle={viewAngle}
                             roomData={data}
-                            highlight={highlightHotspots}
-                            clickHandler={handleHotspotClick}
-                            stopPropogation={!!handleHotspotClick}
-                            handleHover={handleHover}
-                            markVertices={markHotspotVertices.includes(index)}
-                            flash={flashHotspot === index}
                         />
                     )}
 
-                    {contents.map(entry => (
-                        <ActorFigure key={entry.data.id}
-                            isPaused={isPaused}
-                            forPreview={forPreview}
-                            data={entry.data}
-                            orders={entry.orders || []}
-                            clickHandler={entry.clickHandler}
-                            roomData={data}
-                            viewAngle={viewAngle}
-                            roomScale={scale}
-                            handleHover={handleHover}
-                            overrideSprite={entry.overrideSprite}
-                        />
-                    ))}
+                {showObstacleAreas && walkableAreas.map((zone, index) => {
+                    const center = (frameWidth / 2) + getShift(viewAngle, 1, data)
+                    const left = center - data.width / 2
+                    const classes = zone.disabled ? [styles.walkableArea, styles.disabledZone].join(" ") : styles.walkableArea;
+                    return <ZoneSvg key={index}
+                        className={classes}
+                        stopPropagation={false}
+                        zone={zone}
+                        x={zone.x + left}
+                        y={data.height - zone.y}
+                        markVertices={markWalkableVertices.includes(index)}
+                    />
+                })}
 
-                    {background
-                        .filter(layer => layer.parallax > 1)
-                        .map((layer, index) =>
-                            <BackgroundShape key={index}
-                                layer={layer}
-                                viewAngle={viewAngle}
-                                roomData={data}
-                            />
-                        )}
+                {showObstacleAreas && obstacleAreas.map((zone, index) => {
+                    const center = (frameWidth / 2) + getShift(viewAngle, 1, data)
+                    const left = center - data.width / 2
+                    const classes = zone.disabled ? [styles.obstacleArea, styles.disabledZone].join(" ") : styles.obstacleArea;
+                    return <ZoneSvg key={index}
+                        className={classes}
+                        stopPropagation={false}
+                        zone={zone}
+                        x={zone.x + left}
+                        y={data.height - zone.y}
+                        markVertices={markObstacleVertices.includes(index)}
+                    />
+                })}
 
-                    {contents.map(entry => (
-                        <DialogueBubble key={entry.data.id}
-                            actorData={entry.data}
-                            orders={entry.orders || []}
-                            viewAngle={viewAngle}
-                            roomData={data} roomScale={scale} />
-                    ))}
+                {obstacleCells &&
+                    <ObstacleCellOverlay roomData={data} viewAngle={viewAngle} cellMatrix={obstacleCells} />
+                }
 
-                    {children}
-                </svg>
-
-                {showCaption && (
-                    <figcaption className={styles.roomCaption}>{id}</figcaption>
+                {hotspots.map((zone, index) =>
+                    <Hotspot key={index}
+                        zone={zone}
+                        viewAngle={viewAngle}
+                        roomData={data}
+                        highlight={highlightHotspots}
+                        clickHandler={handleHotspotClick}
+                        stopPropogation={!!handleHotspotClick}
+                        handleHover={handleHover}
+                        markVertices={markHotspotVertices.includes(index)}
+                        flash={flashHotspot === index}
+                    />
                 )}
-            </figure>
-        </div>
+
+                {contents.map(entry => (
+                    <ActorFigure key={entry.data.id}
+                        isPaused={isPaused}
+                        forPreview={forPreview}
+                        data={entry.data}
+                        orders={entry.orders || []}
+                        clickHandler={entry.clickHandler}
+                        roomData={data}
+                        viewAngle={viewAngle}
+                        roomScale={scale}
+                        handleHover={handleHover}
+                        overrideSprite={entry.overrideSprite}
+                    />
+                ))}
+
+                {background
+                    .filter(layer => layer.parallax > 1)
+                    .map((layer, index) =>
+                        <BackgroundShape key={index}
+                            layer={layer}
+                            viewAngle={viewAngle}
+                            roomData={data}
+                        />
+                    )}
+
+                {contents.map(entry => (
+                    <DialogueBubble key={entry.data.id}
+                        actorData={entry.data}
+                        orders={entry.orders || []}
+                        viewAngle={viewAngle}
+                        roomData={data} roomScale={scale} />
+                ))}
+
+                {children}
+            </svg>
+
+            {showCaption && (
+                <figcaption className={styles.roomCaption}>{id}</figcaption>
+            )}
+        </figure>
     )
 
 }
