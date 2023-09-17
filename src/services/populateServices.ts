fc50fd32b751e0663d111277644c5658684f78ba
dblatcher <dv.bltchr@gmail.com>
2023-09-17 11:48:47 +0100

    remove sprite service

--- src/services/populateServices.ts
+++ src/services/populateServices.ts
@@ -1,16 +1,11 @@
 import imageService, { ImageAsset } from "./imageService";
 import soundService, { SoundAsset } from "./soundService";
-import spriteService from "./spriteService";
-import { Sprite } from "@/lib/Sprite";
 import { imageAssets } from "../data/images";
 import { soundAssets } from "../data/sounds";
-import { prebuiltGameDesign } from "../data/fullGame";
 import { GameDesign } from "../definitions/Game";
 
 export function populateServicesForPreBuiltGame(): void {
-  const sprites = prebuiltGameDesign.sprites.map((data) => new Sprite(data));
   console.log("populating services");
-  spriteService.add(sprites);
   imageService.add(imageAssets);
   soundService.add(soundAssets);
 }
@@ -21,7 +16,6 @@ export function populateServices(
   soundAssets: SoundAsset[],
 ): void {
   console.log("populating services for:", gameDesign.id);
-  spriteService.add(gameDesign.sprites.map((data) => new Sprite(data)));
   imageService.add(imageAssets);
   soundService.add(soundAssets);
 }
