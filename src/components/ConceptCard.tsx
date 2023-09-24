fbba16497c7ce9acf3c16055e8f34478d848a25d
dblatcher <dv.bltchr@gmail.com>
2023-09-24 16:56:39 +0100

    even better cards

--- src/components/ConceptCard.tsx
+++ src/components/ConceptCard.tsx
@@ -13,20 +13,24 @@ export const ConceptCard = ({ Icon, handleClick, title, description }: Props) =>
 
     return (
         <Card onClick={handleClick}
-            sx={{ maxWidth: 180, minWidth: 180 }}
+            sx={{ maxWidth: 180, minWidth: 180, borderColor:'secondary.light' }}
             variant="outlined"
         >
-            <CardActionArea
-                sx={{ padding: 1 }}
-            >
-                <Box display={'flex'} alignItems={'flex-start'}>
-                    <Icon fontSize="large" color={'secondary'} />
-                    <Box paddingLeft={1} flex={1}>
-                        <Typography variant="caption" borderBottom={1}>{title}</Typography>
-                        <Typography >
-                            {description}
-                        </Typography>
+            <CardActionArea>
+                <Box display={'flex'} alignItems={'center'}
+                    sx={{ backgroundColor: "secondary.light", paddingX: 1, paddingY: .25 }}
+                >
+                    <Icon fontSize="large" color={'inherit'} />
+                    <Box paddingLeft={.5} flex={1}>
+                        <Typography variant="h6" component={'span'}>{title}</Typography>
                     </Box>
+                </Box>
+                <Box
+                    sx={{ paddingX: 1, paddingY: .25 }}
+                >
+                    <Typography >
+                        {description}
+                    </Typography>
                 </Box>
             </CardActionArea>
         </Card >
