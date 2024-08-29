592264856bdb5ac62f822ba1456725f4f862f536
dblatcher <dv.bltchr@gmail.com>
2024-08-29 23:20:42 +0100

    tigheter act step form

--- src/components/GameEditor/OrderForm/OrderWithStepsForm.tsx
+++ src/components/GameEditor/OrderForm/OrderWithStepsForm.tsx
@@ -1,10 +1,13 @@
 
 import { FieldDef, FieldValue, SchemaForm, getModification } from "@/components/SchemaForm";
-import { ActOrder, MoveOrder, stepSchama } from "@/definitions/Order";
+import { ActOrder, ActStep, MoveOrder, stepSchama } from "@/definitions/Order";
 import { Box, Typography } from "@mui/material";
 import { Component } from "react";
 import { ArrayControl } from "../ArrayControl";
 import { makeNewStep } from "../defaults";
+import { StringInput } from "@/components/SchemaForm/StringInput";
+import { NumberInput } from "@/components/SchemaForm/NumberInput";
+import { BooleanInput } from "@/components/SchemaForm/BooleanInput";
 
 type OrderWithSteps = MoveOrder | ActOrder
 
@@ -14,11 +17,39 @@ interface Props {
     updateData: { (data: OrderWithSteps): void };
 }
 
+const ActStepFields = (props: {
+    index: number,
+    animationSuggestions?: string[],
+    changeStep: { (mod: Partial<ActStep>, index: number): void },
+    step: ActStep
+}) => {
+    const { step, index, animationSuggestions, changeStep } = props
+
+    return <>
+        <Box display={'flex'} flexWrap={'wrap'}>
+            <StringInput label="animation" 
+                value={step.animation ?? ''} 
+                suggestions={animationSuggestions} 
+                inputHandler={( animation) => changeStep({animation}, index)} 
+            />
+            <NumberInput label="duration" 
+                value={step.duration} 
+                inputHandler={(duration) => changeStep({duration}, index)} 
+            />
+            <BooleanInput label="reverse" 
+                value={step.reverse ?? false} 
+                inputHandler={(reverse) => changeStep({reverse}, index)} 
+            />
+        </Box>
+      </>
+}
+
 export class OrderWithStepsForm extends Component<Props> {
     constructor(props: Props) {
         super(props)
         this.changeStep = this.changeStep.bind(this)
         this.changeValue = this.changeValue.bind(this)
+        this.changeActStep = this.changeActStep.bind(this)
     }
 
     changeValue(propery: keyof OrderWithSteps, newValue: string | undefined | OrderWithSteps['steps']) {
@@ -56,6 +87,24 @@ export class OrderWithStepsForm extends Component<Props> {
         this.changeValue('steps', stepListCopy as OrderWithSteps['steps'])
     }
 
+    changeActStep(mod: Partial<ActStep>, index: number) {
+        const { data } = this.props
+        if (data.type !== 'act') {
+            return
+        }
+        const stepToUpdate = data.steps[index]
+        if (!stepToUpdate) {
+            return
+        }
+        const updatedStep = {
+            ...stepToUpdate,
+            ...mod,
+        }
+        const stepListCopy = [...data.steps]
+        stepListCopy.splice(index, 1, updatedStep)
+        this.changeValue('steps', stepListCopy)
+    }
+
     render() {
         const { type, steps, } = this.props.data
         const { animationSuggestions } = this.props
@@ -70,15 +119,16 @@ export class OrderWithStepsForm extends Component<Props> {
                     <ArrayControl
                         list={steps}
                         describeItem={(step, index) => (
-                            <SchemaForm key={index}
-                                changeValue={(value, field) => { changeStep(value, field, index) }}
-                                data={step}
-                                suggestions={{ animation: animationSuggestions }}
-                                schema={stepSchama[type]} />
+                            <ActStepFields index={index}
+                                changeStep={this.changeActStep}
+                                step={step}
+                                animationSuggestions={animationSuggestions}
+                            />
                         )}
                         mutateList={(newList) => changeValue('steps', newList)}
                         createItem={makeNewStep[type]}
-                        frame="PLAIN"
+                        buttonSize="small"
+                        frame="NONE"
                     />
                 )}
                 {type === 'move' && (
