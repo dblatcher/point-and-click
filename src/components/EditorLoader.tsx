5b9ab8e21d93f3af85293856807a557c875805fc
dblatcher <dv.bltchr@gmail.com>
2024-12-25 22:56:58 +0000

    update skeleton

--- src/components/EditorLoader.tsx
+++ src/components/EditorLoader.tsx
@@ -1,32 +1,65 @@
 import type { GameEditorProps } from "@/components/GameEditor"
-import { Container, Divider, Skeleton, Stack } from '@mui/material'
+import { editorTheme } from "@/theme"
+import { Box, Container, Skeleton, Stack, ThemeProvider } from '@mui/material'
 import dynamic from 'next/dynamic'
+import { Fragment, ReactNode } from "react"
+
+const Repeat = ({ children, count }: { children: ReactNode, count: number }) => {
+
+  const iteroo: number[] = []
+  iteroo.length = count
+  iteroo.fill(0)
+
+  return <>
+    {iteroo.map((_n, i) => (
+      <Fragment key={i}>
+        {children}
+      </Fragment>
+    ))}
+  </>
+
+}
 
 const DynamicComponent = dynamic<GameEditorProps>(() => import('@/components/GameEditor', {}), {
   loading: () => (<>
-
-    <Container maxWidth='xl'>
-      <Stack
-        direction={'row'}
-        spacing={1}
-        component={'main'}
-        divider={<Divider orientation="vertical" flexItem />}
-      >
+    <ThemeProvider theme={editorTheme}>
+      <Container maxWidth='xl'>
         <Stack
-          component={'nav'}
+          direction={'row'}
           spacing={1}
-          width={150}
+          component={'main'}
         >
-          <Skeleton variant='text' width={'100%'} sx={{ fontSize: 50 }} />
-          <Skeleton variant='rounded' width={'100%'} height={500} />
-        </Stack>
+          <Stack component={'nav'}
+            spacing={1}
+            width={150}
+          >
+            <Stack direction={'row'} spacing={1} marginTop={1}>
+              <Repeat count={4}>
+                <Skeleton variant='circular' width={30} height={30} />
+              </Repeat>
+            </Stack>
+            <Repeat count={13}>
+              <Skeleton variant='rectangular' width={'100%'} height={22} />
+            </Repeat>
+          </Stack>
 
-        <Stack component={'section'} flex={1} spacing={1}>
-          <Skeleton variant='text' width={'100%'} sx={{ fontSize: 50 }} />
-          <Skeleton variant='rounded' width={'100%'} height={650} />
+          <Box component={'section'}
+            flex={1}
+            padding={1}
+          >
+            <Skeleton variant="text" width={120} height={30} />
+            <Box
+              display={'flex'}
+              gap={2}
+            >
+              <Skeleton variant="rectangular" width={160} height={200} />
+              <Skeleton variant="rectangular" width={160} height={400} />
+              <Skeleton variant="rectangular" width={160} height={250} />
+            </Box>
+          </Box>
         </Stack>
-      </Stack>
-    </Container>
+      </Container>
+    </ThemeProvider>
   </>
   ),
   ssr: false,
