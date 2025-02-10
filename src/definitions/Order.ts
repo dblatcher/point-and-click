6f9f695e20255afc8f70e091177bb36f42dbfa1d
dblatcher <dv.bltchr@gmail.com>
2025-02-10 23:02:22 +0000

    do not need stepSchema object

--- src/definitions/Order.ts
+++ src/definitions/Order.ts
@@ -6,18 +6,21 @@ const orderBase = {
     narrative: NarrativeSchema.optional(),
 }
 
+const moveStepSchema = z.object({
+    animation: z.optional(z.string()),
+    x: z.number(),
+    y: z.number(),
+    speed: z.optional(z.number()),
+})
+export type MoveStep = z.infer<typeof moveStepSchema>
+
 export const MoveOrderSchema = z.object({
     ...orderBase,
     type: z.literal('move'),
     roomId: z.string().optional(),
     pathIsSet: z.optional(z.boolean()),
     doPendingInteractionWhenFinished: z.optional(z.boolean()),
-    steps: z.array(z.object({
-        animation: z.optional(z.string()),
-        x: z.number(),
-        y: z.number(),
-        speed: z.optional(z.number()),
-    })),
+    steps: z.array(moveStepSchema),
 })
 export type MoveOrder = z.infer<typeof MoveOrderSchema>
 
@@ -39,15 +42,19 @@ export const SayOrderSchema = z.object({
 })
 export type SayOrder = z.infer<typeof SayOrderSchema>
 
+
+const actStepSchema = z.object({
+    animation: z.optional(z.string()),
+    duration: z.number(),
+    timeElapsed: z.optional(z.number()),
+    reverse: z.optional(z.boolean()),
+})
+export type ActStep = z.infer<typeof actStepSchema>
+
 export const ActOrderSchema = z.object({
     ...orderBase,
     type: z.literal('act'),
-    steps: z.array(z.object({
-        animation: z.optional(z.string()),
-        duration: z.number(),
-        timeElapsed: z.optional(z.number()),
-        reverse: z.optional(z.boolean()),
-    })),
+    steps: z.array(actStepSchema),
 })
 export type ActOrder = z.infer<typeof ActOrderSchema>
 
@@ -55,11 +62,3 @@ export const orderSchema = z.union([MoveOrderSchema, ActOrderSchema, SayOrderSch
 export type Order = z.infer<typeof orderSchema>
 export type OrderType = 'move' | 'act' | 'say' | 'goTo'
 export const orderTypes: OrderType[] = orderSchema.options.map(option => option.shape.type.value)
-
-export const stepSchama = {
-    act: ActOrderSchema.shape.steps.element,
-    move: MoveOrderSchema.shape.steps.element,
-}
-
-export type ActStep = z.infer<typeof stepSchama.act>
-export type MoveStep = z.infer<typeof stepSchama.move>
