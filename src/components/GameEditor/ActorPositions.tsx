f80624447e80f140cede70f3ba74d62090652638
dblatcher <dv.bltchr@gmail.com>
2025-02-25 23:31:41 +0000

    can set actor status

--- src/components/GameEditor/ActorPositions.tsx
+++ src/components/GameEditor/ActorPositions.tsx
@@ -1,17 +1,17 @@
 import { useGameDesign } from "@/context/game-design-context";
-import { ActorData } from "@/definitions";
-import { Dialog, DialogContent, List, ListItemButton, ListItemText } from "@mui/material";
-import { EditorBox } from "./EditorBox";
-import { useState } from "react";
-import { findById } from "@/lib/util";
-import { PositionPreview } from "./ActorEditor/PositionPreview";
+import { getStatusSuggestions } from "@/lib/animationFunctions";
 import { patchMember } from "@/lib/update-design";
+import { findById } from "@/lib/util";
+import { Dialog, DialogContent, DialogTitle, List, ListItemButton, ListItemText } from "@mui/material";
+import { useState } from "react";
+import { StringInput } from "../SchemaForm/StringInput";
+import { PositionPreview } from "./ActorEditor/PositionPreview";
+import { EditorBox } from "./EditorBox";
 
 
 export const ActorPositions = () => {
     const { gameDesign, applyModification } = useGameDesign();
     const [actorId, setActorId] = useState<string | undefined>(undefined)
-
     const actorToPosition = findById(actorId, gameDesign.actors)
 
     return (
@@ -27,15 +27,23 @@ export const ActorPositions = () => {
                 open={!!actorToPosition}
                 onClose={() => setActorId(undefined)}
             >
+                <DialogTitle>{actorToPosition?.name}</DialogTitle>
                 <DialogContent>
-                    {actorToPosition && (
+                    {actorToPosition && (<>
+                        <StringInput optional value={actorToPosition.status ?? ''} label="status"
+                            suggestions={getStatusSuggestions(actorToPosition.id, gameDesign)}
+                            inputHandler={(status) => applyModification(
+                                `set ${actorToPosition.id} status to ${status}`,
+                                { actors: patchMember(actorToPosition.id, { status }, gameDesign.actors) }
+                            )}
+                        />
                         <PositionPreview actorData={actorToPosition} updateFromPartial={(mod) => {
                             applyModification(
                                 `move ${actorToPosition.id}`,
                                 { actors: patchMember(actorToPosition.id, mod, gameDesign.actors) }
                             )
                         }} />
-                    )}
+                    </>)}
                 </DialogContent>
             </Dialog>
         </EditorBox>
