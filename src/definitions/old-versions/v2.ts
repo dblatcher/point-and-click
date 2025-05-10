25177f07d983f25fea2155ab8584191429fc1fef
dblatcher <dv.bltchr@gmail.com>
2025-05-10 11:06:11 +0100

    only export the v2 stuff with a difference

--- src/definitions/old-versions/v2.ts
+++ src/definitions/old-versions/v2.ts
@@ -1,30 +1,18 @@
 import { z } from "zod"
-import { ActorDataSchema, ActorData } from "../ActorData"
-import { ConversationSchema, Conversation } from "../Conversation"
-import { EndingSchema, Ending } from "../Ending"
+import { ActorDataSchema } from "../ActorData"
+import { ConversationSchema } from "../Conversation"
+import { EndingSchema } from "../Ending"
 import { FlagMapSchema } from "../Flag"
 import { InteractionSchema } from "../Interaction"
-import { ItemDataSchema, ItemData } from "../ItemData"
-import { orderSchema } from "../Order"
-import { RoomDataSchema, RoomData } from "../RoomData"
-import { SequenceSchema, Sequence } from "../Sequence"
-import { SpriteDataSchema, SpriteData } from "../SpriteSheet"
-import { StoryBoardSchema, StoryBoard } from "../StoryBoard"
-import { VerbSchema, Verb } from "../Verb"
+import { ItemDataSchema } from "../ItemData"
+import { RoomDataSchema } from "../RoomData"
+import { SequenceSchema } from "../Sequence"
+import { SpriteDataSchema } from "../SpriteSheet"
+import { StoryBoardSchema } from "../StoryBoard"
+import { VerbSchema } from "../Verb"
 
 
-
-const GameHappeningsSchema = z.object({
-    sequenceRunning: SequenceSchema.optional(),
-    currentStoryBoardId: z.string().optional(),
-    actorOrders: z.record(z.string(), orderSchema.array()),
-    currentConversationId: z.string().optional(),
-    endingId: z.string().optional(),
-    pendingInteraction: InteractionSchema.optional(),
-    gameNotBegun: z.boolean(),
-})
-
-export const GameContentsDataSchema = z.object({
+const GameContentsDataSchema = z.object({
     rooms: RoomDataSchema.array(),
     items: ItemDataSchema.array(),
     actors: ActorDataSchema.array(),
@@ -37,7 +25,7 @@ export const GameContentsDataSchema = z.object({
 })
 
 
-export const FixedGameInfoSchema = z.object({
+const FixedGameInfoSchema = z.object({
     verbs: VerbSchema.array(),
     sequences: SequenceSchema.array(),
     sprites: SpriteDataSchema.array(),
@@ -46,34 +34,6 @@ export const FixedGameInfoSchema = z.object({
     openingStoryboardId: z.string().optional(),
     storyBoards: StoryBoardSchema.array().optional(),
 })
-export const GameDataSchema = GameContentsDataSchema.and(GameHappeningsSchema)
-const GameConditionSchema = GameContentsDataSchema.and(GameHappeningsSchema).and(FixedGameInfoSchema)
+
 export const v2GameDesignSchema = GameContentsDataSchema.and(FixedGameInfoSchema).describe('The game schema')
-
-//                  GameData GameCondition GameDesign GameContents
-// GameHappenings  |   x    |   x        |           |
-// GameContentsData|   x    |   x        |  x        |  x
-// FixedGameInfo   |        |   x        |  x        |
-
-export type GameContents = z.infer<typeof GameContentsDataSchema>
-export type GameData = z.infer<typeof GameDataSchema>
-export type FixedGameInfo = z.infer<typeof FixedGameInfoSchema>
-export type GameCondition = z.infer<typeof GameConditionSchema>
 export type V2GameDesign = z.infer<typeof v2GameDesignSchema>
-
-export type GameDataItem = ActorData | ItemData | Conversation | RoomData | SpriteData | Sequence | Ending | Verb| StoryBoard
-
-export const GameDataItemTypeEnum = z.enum([
-    'rooms', 'items', 'actors', 'conversations', 'sprites', 'sequences', 'endings', 'verbs', 'storyBoards'
-])
-
-export type GameDataItemType =
-    'rooms'
-    | 'items'
-    | 'actors'
-    | 'conversations'
-    | 'sprites'
-    | 'sequences'
-    | 'endings'
-    | 'verbs'
-    | 'storyBoards'
