aea455b7ba954450d328653b00045d802b940ec4
dblatcher <dv.bltchr@gmail.com>
2025-07-23 22:34:04 +0100

    move the subtypes

--- src/definitions/Consequence.ts
+++ src/definitions/Consequence.ts
@@ -1,125 +1,14 @@
 import { z } from "zod"
-import { Order, orderSchema } from "./Order"
-import { Narrative, NarrativeSchema } from "./BaseTypes"
+import { Narrative } from "./BaseTypes"
+import { AmbientNoiseConsequenceSchema, BackgroundMusicConsequenceSchema, ChangeRoomConsequenceSchema, ChangeStatusConsequenceSchema, ConversationChoiceConsequenceSchema, ConversationConsequenceSchema, FlagConsequenceSchema, InventoryConsequenceSchema, OrderConsequenceSchema, RemoveActorConsequenceSchema, SequenceConsequenceSchema, SoundEffectConsequenceSchema, StoryBoardConsequenceSchema, TeleportActorConsequenceSchema, ToggleZoneConsequenceSchema } from "./consequence-subtypes"
 import { EndingConsequenceSchema } from "./old-versions/deprecated-schemas"
+import { Order } from "./Order"
 
-const OrderConsequenceSchema = z.object({
-    type: z.literal('order'),
-    actorId: z.string().optional(),
-    orders: z.array(orderSchema),
-    replaceCurrentOrders: z.optional(z.boolean()),
-    narrative: NarrativeSchema.optional(),
-})
 export type OrderConsequence = z.infer<typeof OrderConsequenceSchema>
 
-const ChangeRoomConsequenceSchema = z.object({
-    type: z.literal('changeRoom'),
-    roomId: z.string(),
-    takePlayer: z.boolean(),
-    x: z.optional(z.number()),
-    y: z.optional(z.number()),
-    narrative: NarrativeSchema.optional(),
-})
-
-const InventoryConsequenceSchema = z.object({
-    type: z.literal('inventory'),
-    itemId: z.string(),
-    actorId: z.string().optional(),
-    addOrRemove: z.enum(['ADD', 'REMOVE']),
-    narrative: NarrativeSchema.optional(),
-})
-
-const RemoveActorConsequenceSchema = z.object({
-    type: z.literal('removeActor'),
-    actorId: z.string(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const ChangeStatusConsequenceSchema = z.object({
-    type: z.literal('changeStatus'),
-    targetId: z.string(),
-    targetType: z.enum(['actor', 'item', 'hotspot']),
-    status: z.string(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const SequenceConsequenceSchema = z.object({
-    type: z.literal('sequence'),
-    sequence: z.string(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const ConversationConsequenceSchema = z.object({
-    type: z.literal('conversation'),
-    conversationId: z.string(),
-    end: z.optional(z.boolean()),
-    narrative: NarrativeSchema.optional(),
-})
-
-const TeleportActorConsequenceSchema = z.object({
-    type: z.literal('teleportActor'),
-    actorId: z.string(),
-    roomId: z.optional(z.string()),
-    x: z.number(),
-    y: z.number(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const ToggleZoneConsequenceSchema = z.object({
-    type: z.literal('toggleZone'),
-    roomId: z.optional(z.string()),
-    on: z.boolean(),
-    ref: z.string(),
-    zoneType: z.enum(['hotspot', 'obstacle', 'walkable']),
-    narrative: NarrativeSchema.optional(),
-})
 export type ZoneType = z.infer<typeof ToggleZoneConsequenceSchema.shape.zoneType>
 export const zoneTypes: ZoneType[] = ToggleZoneConsequenceSchema.shape.zoneType.options
 
-const SoundEffectConsequenceSchema = z.object({
-    type: z.literal('soundEffect'),
-    sound: z.string(),
-    volume: z.number().optional(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const BackgroundMusicConsequenceSchema = z.object({
-    type: z.literal('backgroundMusic'),
-    sound: z.string().optional(),
-    roomId: z.string().optional(),
-    volume: z.number().optional(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const AmbientNoiseConsequenceSchema = z.object({
-    type: z.literal('ambientNoise'),
-    sound: z.string().optional(),
-    roomId: z.string().optional(),
-    volume: z.number().optional(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const FlagConsequenceSchema = z.object({
-    type: z.literal('flag'),
-    on: z.boolean(),
-    flag: z.string(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const ConversationChoiceConsequenceSchema = z.object({
-    type: z.literal('conversationChoice'),
-    on: z.boolean(),
-    conversationId: z.string(),
-    branchId: z.string(),
-    choiceRef: z.string(),
-    narrative: NarrativeSchema.optional(),
-})
-
-const StoryBoardConsequenceSchema = z.object({
-    type: z.literal('storyBoardConsequence'),
-    storyBoardId: z.string(),
-    narrative: NarrativeSchema.optional(),
-})
 
 export const ConsequenceSchema = z.union([
     OrderConsequenceSchema,
