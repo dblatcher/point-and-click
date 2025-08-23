cf9deef0c4a6b5a4c4ecc60bf53792784dcccf8c
dblatcher <dv.bltchr@gmail.com>
2025-08-23 22:05:55 +0100

    homepage component

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,37 @@
+import { Box, Card, Grid, Stack } from "@mui/material"
+import { MarkDown } from "./MarkDown"
+import content from "@/content/homepage.md";
+import featuresAndDetails from "@/content/featuresAndDetails.md";
+import { LinkButton } from "./LinkButton";
+
+export const Homepage = () => {
+
+    return <>
+        <Grid container spacing={2} padding={2}
+            justifyContent="center"
+            alignItems="center">
+            <Grid item xs={6}>
+                <Card sx={{
+                    padding: 2,
+                }}>
+                    <MarkDown content={content} />
+                </Card>
+            </Grid>
+            <Grid item xs={6}>
+                <Stack gap={2}>
+                    <LinkButton href="./game-loader" variant='contained'>game loader</LinkButton>
+                    <LinkButton href="./prebuilt-game" variant='contained'>prebuilt game</LinkButton>
+                    <LinkButton href="./editor" variant='contained'>game designer</LinkButton>
+                </Stack>
+            </Grid>
+        </Grid>
+
+        <Box padding={2}>
+            <Card sx={{
+                padding: 2,
+            }}>
+                <MarkDown content={featuresAndDetails} />
+            </Card>
+        </Box>
+    </>
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,10 @@
-import { LinkButton } from '@/components/LinkButton';
 import { PageLayout } from '@/components/PageLayout';
-import { Box, Card, Grid, Stack } from '@mui/material';
-
-import { MarkDown } from '@/components/MarkDown';
-import featuresAndDetails from "@/content/featuresAndDetails.md";
-import content from "@/content/homepage.md";
-
+import { Homepage } from '@/components/Homepage';
 
 export default function Home() {
   return (
     <PageLayout>
-
-      <Grid container spacing={2} padding={2}
-        justifyContent="center"
-        alignItems="center">
-        <Grid item xs={6}>
-          <Card sx={{
-            padding: 2,
-          }}>
-            <MarkDown content={content} />
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Stack gap={2}>
-            <LinkButton href="./game-loader" variant='contained'>game loader</LinkButton>
-            <LinkButton href="./prebuilt-game" variant='contained'>prebuilt game</LinkButton>
-            <LinkButton href="./editor" variant='contained'>game designer</LinkButton>
-          </Stack>
-        </Grid>
-      </Grid>
-
-      <Box padding={2}>
-        <Card sx={{
-          padding: 2,
-        }}>
-          <MarkDown content={featuresAndDetails} />
-        </Card>
-      </Box>
-
+      <Homepage />
     </PageLayout >
   )
 }
