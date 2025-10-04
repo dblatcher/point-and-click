b1c91ce8640d963380864a4ed5d416587eb27162
dblatcher <dv.bltchr@gmail.com>
2025-10-04 09:04:12 +0100

    responsive layout and better copy

--- src/components/GameEditor/tutorial/TutorialContent.tsx
+++ src/components/GameEditor/tutorial/TutorialContent.tsx
@@ -1,6 +1,6 @@
 import { useGameDesign } from "@/context/game-design-context"
 import { TutorialStage } from "@/lib/game-design-logic/types"
-import { Box, Button, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material"
+import { Box, Button, Grid, List, ListItem, ListItemIcon, ListItemText, Typography } from "@mui/material"
 import { CheckBoxIcon, CheckBoxOutlineBlankIcon } from "../material-icons"
 
 interface Props {
@@ -16,33 +16,43 @@ export const TutorialContent = ({ currentStage, onLastStage, progressToNextStage
     const stageComplete = currentStage.tasks.every(task => task.test(editorState))
 
     return <Box padding={1}>
-        {currentStage && (
+        {currentStage.tasks.length > 0 ? (
 
+            <Grid container>
+                <Grid item xs={12} lg={4}>
+                    {currentStage.subtitle && <Typography variant="h3">{currentStage.subtitle}</Typography>}
+                    {currentStage.intro}
+                </Grid>
+                <Grid item xs={12} lg={8}>
+                    <List>
+                        {currentStage.tasks.map((task, index) => (
+                            <ListItem key={index}>
+                                <ListItemText primary={task.title} secondary={task.detail} />
+                                <ListItemIcon>
+                                    {task.test(editorState) ? <CheckBoxIcon />
+                                        : <CheckBoxOutlineBlankIcon />}
+                                </ListItemIcon>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Grid>
+            </Grid>
+        ) : (
             <Box>
                 {currentStage.subtitle && <Typography variant="h3">{currentStage.subtitle}</Typography>}
                 {currentStage.intro}
-
-                <List>
-                    {currentStage.tasks.map((task, index) => (
-                        <ListItem key={index}>
-                            <ListItemText primary={task.title} secondary={task.detail} />
-                            <ListItemIcon>
-                                {task.test(editorState) ? <CheckBoxIcon />
-                                    : <CheckBoxOutlineBlankIcon />}
-                            </ListItemIcon>
-                        </ListItem>
-                    ))}
-                </List>
-                {stageComplete && (
-                    <Box>
-                        <Typography>{currentStage.confirmation}</Typography>
-                        {!onLastStage && (
-                            <Button onClick={progressToNextStage}>next</Button>
-                        )}
-                    </Box>
-                )}
             </Box>
         )}
-    </Box>
+        <Box>
+            {stageComplete && (
+                <Box>
+                    <Typography>{currentStage.confirmation}</Typography>
+                    {!onLastStage && (
+                        <Button onClick={progressToNextStage}>next</Button>
+                    )}
+                </Box>
+            )}
+        </Box>
+    </Box >
 
 }
--- src/lib/tutorials/basic.tsx
+++ src/lib/tutorials/basic.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material"
 import { Tutorial } from "../game-design-logic/types"
-import { InteractionIcon, PlayCircleFilledOutlinedIcon } from "@/components/GameEditor/material-icons"
+import { HelpIcon, InteractionIcon, PlayCircleFilledOutlinedIcon } from "@/components/GameEditor/material-icons"
 
 export const basicTutorial: Tutorial = {
     title: 'Learning the basics',
@@ -13,19 +13,20 @@ export const basicTutorial: Tutorial = {
                     To get you started, the game already has already a room with your player character and an NPC, but if you test the game with the Play Button <PlayCircleFilledOutlinedIcon fontSize="inherit"/> and try to talk to them, the game will just give you a default response.
                 </Typography>
                 <Typography>
-                    Our first task will be adding an INTERACTION <InteractionIcon fontSize="inherit"/> so something happens when you talk to the NPC.
+                    Our first task will be adding an INTERACTION <InteractionIcon fontSize="inherit"/> so something happens when you talk to the NPC. Not sure what an INTERACTION is? Try clicking the help icon <HelpIcon fontSize="inherit"/> on the Interactions screen.
                 </Typography>
             </>),
             tasks: [
                 {
-                    title: 'open the interactions menu',
+                    title: 'Open the "Interactions" screen',
                     detail: 'click the menu on the left or use the 5 key',
                     test(state) {
                         return state.tabOpen === 'interactions'
                     },
                 },
                 {
-                    title: 'add an interaction for TALK (verb) NPC (target)',
+                    title: 'Add an INTERACTION for TALK (verb) NPC (target)',
+                    detail: 'click "add new interaction", set the verb and target in the dialog and save changes',
                     test(state) {
                         return state.gameDesign.interactions.some(
                             interaction => interaction.targetId === 'NPC' &&
@@ -34,12 +35,16 @@ export const basicTutorial: Tutorial = {
                     },
                 },
                 {
-                    title: 'create a consequence for the interaction - give the PLAYER an ORDER to say something - anything you like!',
+                    title: 'Give the PLAYER something to say',
+                    detail:'open your INTERACTION with the edit button, add an "order" CONSEQUENCE, select "PLAYER" as the ACTOR and create a "say" order. You can then click on the box to edit the text.',
                     test(state) {
                         return state.gameDesign.interactions.some(
                             interaction => interaction.targetId === 'NPC' &&
                                 interaction.verbId === 'TALK' &&
-                                interaction.consequences.some(consequence => consequence.type === 'order' && consequence.orders.some(order => order.type === 'say'))
+                                interaction.consequences.some(consequence => 
+                                    consequence.type === 'order' && 
+                                    consequence.orders.some(order => order.type === 'say'
+                                ))
                         )
                     }
                 }
@@ -52,9 +57,9 @@ export const basicTutorial: Tutorial = {
             </>)
         },
         {
-            intro: 'next stage',
+            intro: <Typography>This is the end of the tutorial</Typography>,
             tasks: [],
-            confirmation: "That's all",
+            confirmation: <Typography>Well done for doing all the tasks.</Typography>,
         }
     ]
 }
