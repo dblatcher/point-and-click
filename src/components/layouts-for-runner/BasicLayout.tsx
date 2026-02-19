66aea83c843d49665c0660b5244df89f9e25ddf3
dblatcher <dv.bltchr@gmail.com>
2026-02-19 21:56:33 +0000

    add in a camera provider

--- src/components/layouts-for-runner/BasicLayout.tsx
+++ src/components/layouts-for-runner/BasicLayout.tsx
@@ -1,5 +1,5 @@
 import { findById } from "@/lib/util"
-import { ContextualGameRoom, GameDataContext } from "point-click-components"
+import { CameraPointProvider, ContextualGameRoom, GameDataContext } from "point-click-components"
 import { useContext } from "react"
 import { StoryBoardPlayer } from "../storyboard/StoryBoardPlayer"
 import { CommandLine } from "./CommandLine"
@@ -7,6 +7,9 @@ import { ItemMenu } from "./ItemMenu"
 import { VerbMenu } from "./VerbMenu"
 import { getUiCondition } from "./helpers"
 import { ConversationMenu } from "./ConversationMenu"
+import { useCamera } from "@/hooks/useCamera"
+import { useInterval } from "@/hooks/useInterval"
+import { getPointOfFocus } from "point-click-lib"
 
 
 export const BasicLayout = () => {
@@ -15,34 +18,44 @@ export const BasicLayout = () => {
     const storyBoard = findById(currentStoryBoardId, gameDesign.storyBoards)
 
     const condition = getUiCondition(gameState)
+    const { updateCamera, cameraPoint } = useCamera()
+
+    // TO DO - should this be built into GameRunner?
+    const moveCamera = () => {
+        const { x, y } = getPointOfFocus(gameState) ?? { x: gameState.viewAngleX, y: gameState.viewAngleY }
+        updateCamera(x, y, gameState.currentRoomId)
+    }
+    useInterval(moveCamera, 10)
 
     return <section>
-        <p>game runner!</p>
-        {condition === 'story-board'
-            ? (
-                <div>
-                    {storyBoard
-                        ? <StoryBoardPlayer storyBoard={storyBoard} />
-                        : <button onClick={() => dispatch({ type: 'CLEAR-STORYBOARD' })}>clear</button>
-                    }
-                </div>
-            ) : (
-                <div>
-                    <ContextualGameRoom />
+        <CameraPointProvider value={{ cameraPoint }}>
+            <p>game runner!</p>
+            {condition === 'story-board'
+                ? (
+                    <div>
+                        {storyBoard
+                            ? <StoryBoardPlayer storyBoard={storyBoard} />
+                            : <button onClick={() => dispatch({ type: 'CLEAR-STORYBOARD' })}>clear</button>
+                        }
+                    </div>
+                ) : (
+                    <div>
+                        <ContextualGameRoom />
 
-                    {condition === 'verbs' && (
-                        <section>
-                            <CommandLine />
-                            <VerbMenu />
-                            <ItemMenu />
-                        </section>
-                    )}
-                    {condition === 'conversation' && (
-                        <section>
-                            <ConversationMenu />
-                        </section>
-                    )}
-                </div>
-            )}
+                        {condition === 'verbs' && (
+                            <section>
+                                <CommandLine />
+                                <VerbMenu />
+                                <ItemMenu />
+                            </section>
+                        )}
+                        {condition === 'conversation' && (
+                            <section>
+                                <ConversationMenu />
+                            </section>
+                        )}
+                    </div>
+                )}
+        </CameraPointProvider>
     </section>
 }
