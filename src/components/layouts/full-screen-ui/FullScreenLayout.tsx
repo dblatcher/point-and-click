b886c4b358ddec03b211ad1bc641f05740877424
dblatcher <dv.bltchr@gmail.com>
2026-02-22 08:36:55 +0000

    iclude storyboard on full screen layout

--- src/components/layouts/full-screen-ui/FullScreenLayout.tsx
+++ src/components/layouts/full-screen-ui/FullScreenLayout.tsx
@@ -3,35 +3,46 @@ import { FullScreenWrapper } from "../../FullScreenWrapper";
 import { DialogSaveMenu } from "../shared-mui-components/DialogSaveMenu";
 import { ConversationMenu } from "../basic/ConversationMenu";
 import { RoomWrapperWithOverlay } from "./RoomWrapperWithOverlay";
+import { StoryBoardPlayer } from "@/components/storyboard/StoryBoardPlayer";
 
 export const FullScreenLayout = () => {
-    const { isConversationRunning, isSequenceRunning } = useGameStateDerivations()
+    const { isConversationRunning, isSequenceRunning, currentStoryBoard } = useGameStateDerivations()
 
     return (
         <FullScreenWrapper>
-            <main style={{
-                flex: 1,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center'
-            }}>
-                <div style={{
-                    position: 'fixed',
-                    top: 0,
-                    right: 0
+            {currentStoryBoard
+                ? <main
+                    style={{
+                        position: 'fixed',
+                        inset: 0,
+                    }}
+                >
+                    <StoryBoardPlayer storyBoard={currentStoryBoard} />
+                </main>
+                : <main style={{
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center'
                 }}>
-                    <DialogSaveMenu />
-                </div>
-                <RoomWrapperWithOverlay />
-                {!isSequenceRunning && (
                     <div style={{
-                        position: 'absolute',
-                        bottom: 0,
+                        position: 'fixed',
+                        top: 0,
+                        right: 0
                     }}>
-                        {isConversationRunning ? <ConversationMenu /> : <></>}
+                        <DialogSaveMenu />
                     </div>
-                )}
-            </main>
+                    <RoomWrapperWithOverlay />
+                    {!isSequenceRunning && (
+                        <div style={{
+                            position: 'absolute',
+                            bottom: 0,
+                        }}>
+                            {isConversationRunning ? <ConversationMenu /> : <></>}
+                        </div>
+                    )}
+                </main>
+            }
         </FullScreenWrapper>
     )
 }
